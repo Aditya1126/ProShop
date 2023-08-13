a3b6eedaf20f8a17da56b1f908530435a56d5d22
adityaadda247 <aditya.gupta@adda247.com>
2023-08-13 22:14:01 +0530

    add code for homescreen and product screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './assets/styles/bootstrap.custom.css'
+import './assets/styles/index.css'
+import App from './App'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route index={false} path='/product/:id' element={<ProductScreen />} />
+    </Route>
+  )
+)
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
   document.getElementById('root')
-);
+)
+
+// serviceWorker.unregister()
